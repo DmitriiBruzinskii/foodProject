39e9a1404c0032be23a30cd6b8c90f33950f6568
dmitrii bruzinskii <bruzinschi_dmitrii@mail.ru>
2023-12-02 10:11:37 +0200

    add slider

--- js/script.js
+++ js/script.js
@@ -254,9 +254,46 @@ window.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }, 4000);
     };
+
+    // Slider
+
+    let slideIndex = 1;
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          total = document.querySelector('#total'),
+          current = document.querySelector('#current');
+
+    showSlides(slideIndex);
+    
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        };
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        };
+
+        slides.forEach(slide => slide.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    };
+
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    };
+
+    prev.addEventListener('click', () => plusSlides(-1));
+
+    next.addEventListener('click', () => plusSlides(1));
 });
-
-
-
 
 
